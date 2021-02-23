d2f7aa29f27f5620406cda5bc0bd4198514186aa
Valentin Klein <valentin.klein@student.uclouvain.be>
2021-02-23 12:39:58 +0100

    SignupActivity.java le commencement

--- android-studio/app/src/main/java/com/example/p4_group12/MainActivity.java
+++ android-studio/app/src/main/java/com/example/p4_group12/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.p4_group12;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import com.example.p4_group12.database.DatabaseContact;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- android-studio/app/src/main/java/com/example/p4_group12/Signup.java
+++
@@ -1,7 +0,0 @@
-package com.example.p4_group12;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Signup extends AppCompatActivity {
-
-}
