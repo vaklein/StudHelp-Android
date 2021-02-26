30722cd724bbc0ff970f10897a55b994d23a2285
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2021-02-26 08:56:45 +0100

    added the activity to show the advertisments

---
+++ android-studio/app/src/main/java/com/example/p4_group12/Interface/AdvertismentsListActivity.java
@@ -0,0 +1,16 @@
+package com.example.p4_group12.Interface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.p4_group12.R;
+
+import android.os.Bundle;
+
+public class AdvertismentsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisments_list);
+    }
+}
