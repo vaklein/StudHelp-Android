6ebccd929b2ac2046b2edaba5216cc9ebd184fab
Julesuisse <jules.lesuisse@student.uclouvain.be>
2021-02-26 09:25:06 +0100

    new files

---
+++ android-studio/app/src/main/java/com/example/p4_group12/DAO/Advertisement.java
@@ -0,0 +1,4 @@
+package com.example.p4_group12.DAO;
+
+public class Advertisement {
+}
---
+++ android-studio/app/src/main/java/com/example/p4_group12/Interface/adapter/AdvertisementListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.p4_group12.Interface.adapter;
+
+public class AdvertisementListAdapter {
+}
