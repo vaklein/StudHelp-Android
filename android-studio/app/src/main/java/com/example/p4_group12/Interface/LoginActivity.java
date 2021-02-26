ece3dc752805358d48e14ec6d0540cec10ae5810
Valentin Klein <valentin.klein@student.uclouvain.be>
2021-02-26 11:39:47 +0100

    Connexion fonctionnelle

--- android-studio/app/src/main/java/com/example/p4_group12/Interface/LoginActivity.java
+++ android-studio/app/src/main/java/com/example/p4_group12/Interface/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.p4_group12.Interface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,21 @@ import com.example.p4_group12.R;
 import com.example.p4_group12.database.DatabaseContact;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class LoginActivity extends AppCompatActivity {
     private Button sign_up;
@@ -59,15 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                 loginField.setErrorEnabled(false);
                 passwordField.setErrorEnabled(false);
                 if (isCorrectlyFil()) {
-                    if (DatabaseContact.connect(login.getText().toString(), password.getText().toString())){
-                        GlobalVariables.setUser(login.getText().toString());
-                        //Intent menu = new Intent(getApplicationContext(), MenuActivity.class);
-                        //startActivity(menu);
-                    }
-                    else{
-                        loginField.setError("Identifiant/Mot de passe incorrect");
-                        passwordField.setError("Identifiant/Mot de passe incorrect");
-                    }
+                    new AsyncLogin().execute(login.getText().toString(),password.getText().toString());
                 }
             }
         });
@@ -85,5 +94,72 @@ public class LoginActivity extends AppCompatActivity {
         }
         return filled;
     }
+    class AsyncLogin extends AsyncTask<String, Void, String> { // Il faut lancer un autre thread car une requete sur le main thread peut faire crasher l'app
 
+        // a modifier en executor si on veut update l'app, asynctask deprecated
+        ProgressDialog pdLoading = new ProgressDialog(LoginActivity.this);
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL("https://db.valentinklein.eu:8182/connect.php");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");  //POST request
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String data = URLEncoder.encode("login", "UTF-8") + "=" + URLEncoder.encode(params[0], "UTF-8") + "&" +
+                        URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(params[1], "UTF-8");//Build form answer
+                bufferedWriter.write(data); //Send data
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream IS = httpURLConnection.getInputStream(); //DB answer
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(IS));
+                String json;
+                StringBuilder result = new StringBuilder();
+                while ((json = bufferedReader.readLine()) != null) {
+                    result.append(json + "\n");
+                }
+                IS.close();
+                httpURLConnection.disconnect();
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            pdLoading.dismiss();
+            try {
+                JSONObject response = new JSONObject(result);
+                JSONObject object = response.getJSONObject("response");
+                if (object.getBoolean("error")) {
+                    loginField.setError("Identifiant/Mot de passe incorrect");
+                    passwordField.setError("Identifiant/Mot de passe incorrect");
+                } else if (object.getBoolean("Logged")) {
+                    Intent intent = new Intent(LoginActivity.this, CourseListActivity.class);
+                    startActivity(intent);
+                    LoginActivity.this.finish();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
